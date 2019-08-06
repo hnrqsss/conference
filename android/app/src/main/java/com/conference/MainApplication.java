3591fff4e5e2a5a647b4c7d267e3a94a76aa5d84
hnrqassuncao <henriqueassuncao@febracis.com.br>
2019-08-06 15:05:33 -0300

    setup react-native-jitsi-meet for android

--- android/app/src/main/java/com/conference/MainApplication.java
+++ android/app/src/main/java/com/conference/MainApplication.java
@@ -7,9 +7,11 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativejitsimeet.JitsiMeetPackage;
 
 import java.util.Arrays;
 import java.util.List;
+import javax.annotation.Nullable;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,13 +24,19 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new JitsiMeetPackage()
       );
     }
 
     @Override
     protected String getJSMainModuleName() {
       return "index";
+    }
+    
+    @Override
+    protected @Nullable String getBundleAssetName() {
+      return "app.bundle";
     }
   };
 
